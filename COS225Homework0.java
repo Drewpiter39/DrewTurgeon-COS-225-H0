887132a9b44cef8e7b457fc30ddcc7d03e41cd7c
Drewpiter39 <drew.turgeon@maine.edu>
2025-01-27 15:07:47 -0500

    Update COS225Homework0.java

--- COS225Homework0.java
+++ COS225Homework0.java
@@ -1,3 +1,14 @@
+// File: COS225Homework0.java
+// Author: Drew Turgeon
+// Date: 1/27/2025
+// Section: 0001
+// E-mail: drew.turgeon@maine.edu
+// Description: 
+// Homework 0, writing hello world and my name in java
+// Collaboration:
+// I didn't collaborate with anyone.
+
+
 public class COS225Homework0{
     public static void main(String[] args){
         System.out.println("Hello World");
